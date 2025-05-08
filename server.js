4a1fb3f9c9b0c13ca99c462631da6329c0eba9df
poojaabachi <poojaabachi@gmail.com>
2025-05-08 12:15:02 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
+
+const connectDB = require("./config/db");
+const errorHandler = require("./middlewares/errorHandler");
+
+const authRoutes = require("./routes/authRoutes");
+const productRoutes = require("./routes/productRoutes");
+const cartRoutes = require ("./routes/CartRoutes");
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Server is running",
+  });
+});
+
+app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+connectDB();
+app.listen(PORT, () => {
+  console.log(`Server is running in ${PORT}`);
+});
